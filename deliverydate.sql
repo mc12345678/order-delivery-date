15825aa37a9be9e3806ff4d584e11e5b487e28e1
mc12345678 <mc12345678@users.noreply.github.com>
2018-09-06 14:27:34 -0400

    Add default date of '0001-01-01'
    There are some database versions that require date fields to have a default value.  This commit adds the definition to the creation of the order_delivery_date field.

--- deliverydate.sql
+++ deliverydate.sql
@@ -1,3 +1,4 @@
 ALTER TABLE orders ADD order_delivery_date date AFTER ip_address;
+ALTER TABLE orders ALTER order_delivery_date SET DEFAULT '0001-01-01';
 
 INSERT INTO configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) VALUES ('Require Order Delivery Date?', 'MIN_DISPLAY_DELIVERY_DATE', 1, 'Any number over zero will make the delivery date a required field in checkout', 2, 18, now());
